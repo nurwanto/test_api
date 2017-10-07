4ee76f1e323bcc7cfd8b20570088b38781867052
nurwanto <nurwanto16@gmail.com>
2017-10-07 15:12:17 +0700

    first commit

---
+++ wwwroot/callback.php
@@ -0,0 +1,25 @@
+<?php // callback.php
+define("LINE_MESSAGING_API_CHANNEL_SECRET", 'fba8c92373f40f0bc056965e9c316cae');
+define("LINE_MESSAGING_API_CHANNEL_TOKEN", '+yU1saW7vhNpawhuXNPhJwE5pUoAOgyrcKCwMh0jz26ttYZUiKKgHNqPH9pmU0+WULaWgFitNyZby06p4rXqn0hV/yT71YQp6wydpXhdyShCVyyrtsZ3Lf3ZzjEc6ErrCC17qkcIHzZBU7FdxowikwdB04t89/1O/w1cDnyilFU=');
+
+require __DIR__."/../vendor/autoload.php";
+
+$bot = new \LINE\LINEBot(
+    new \LINE\LINEBot\HTTPClient\CurlHTTPClient(LINE_MESSAGING_API_CHANNEL_TOKEN),
+    ['channelSecret' => LINE_MESSAGING_API_CHANNEL_SECRET]
+);
+
+$signature = $_SERVER["HTTP_".\LINE\LINEBot\Constant\HTTPHeader::LINE_SIGNATURE];
+$body = file_get_contents("php://input");
+
+$events = $bot->parseEventRequest($body, $signature);
+
+foreach ($events as $event) {
+    if ($event instanceof \LINE\LINEBot\Event\MessageEvent\TextMessage) {
+        $reply_token = $event->getReplyToken();
+        $text = $event->getText();
+        $bot->replyText($reply_token, $text);
+    }
+}
+
+echo "OK";
